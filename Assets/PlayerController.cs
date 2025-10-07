8f0efe6e6026a4e1b4b01a5d226bfb454f47428f
jay-tell3 <jaybroww@gmail.com>
2025-10-07 15:07:21 -0500

    done

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,9 +8,15 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround;
     public bool gameOver = false;
     private Animator playerAnim;
+    public ParticleSystem exp;
+    public ParticleSystem dp;
+    public AudioClip jump;
+    public AudioClip death;
+    public AudioSource audio;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        audio = GetComponent<AudioSource>();
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
@@ -19,11 +25,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (gameOver)
+        {
+            dp.Stop();
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dp.Stop();
+            audio.PlayOneShot(jump, 1.0f);
         }
     }
 
@@ -32,11 +44,18 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dp.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            exp.Play();
+            audio.PlayOneShot(death, 1.0f);
+            dp.Stop();
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b",true);
+            playerAnim.SetInteger("DeathType_int", 1);
+           
         }
     }
 }
